d6f6fecef75a5f4d387a44a01cf67280f8df0f2b
ilker11 <ilker01@abv.bg>
2024-02-08 15:08:47 +0200

    add files

---
+++ src/products.js
@@ -0,0 +1,39 @@
+import product1 from './assets/iphone.jpeg'
+import product2 from './assets/samsung.jpg'
+import product3 from  './assets/huawei.jpg'
+import product4 from './assets/canon.jpg'
+import product5 from './assets/speaker.jpg' 
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: product1 ,
+    },
+    {
+        id: 2,
+        productName: "Samsung",
+        price: 799.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Huawei",
+        price: 599.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Canon",
+        price: 499.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Speaker",
+        price: 99.0,
+        productImage: product5,
+    }
+
+]
